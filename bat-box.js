a596804e77bdb02f8d087513e67544ef7dd4676b
rektide <rektide@voodoowarez.com>
2018-01-07 23:33:25 -0500

    Constants aren't variables- they're constant. Make "expressions".

--- bat-box.js
+++ bat-box.js
@@ -1,26 +1,28 @@
 "use strict"
 
 var
-  Cassowary = require( "cassowary"),
+  c= require( "cassowary"),
   diag= Math.pow( 3, 0.5)
 
 /**
 * Return how circles of radius r fit into an x,y sized rectangle
 */
-function fit( r, x, y){
+function fit( d, x, y){
+	d= new c.Expression( d)
+	x= new c.Expression( x)
+	y= new c.Expression( y)
 	// find columns
 	var
-	  r= new Cassowary.Variable({ value: r, name: "r"}),
-	  d= new Cassowary.Variable({ value: r* 2, name: "d"}),
-	  containerX= new Cassowary.Variable({ value: x, name: "cX"}),
-	  cols= new Cassowary.SlackVariable(),
-	  colIneq= Cassowary.Inequality( Cassowary.divide(containerX, d), Cassowary.GEQ, cols),
-	  colSolver= new Cassowary.SimplexSolver()
-	solver.autoSolve= true
-	solver.addConstraint( colIneq)
+	  cols= new c.Variable({ name: "cols"}),
+	  colEq= new c.Equation( c.times( d, cols), x, c.Strength.required, 0),
+	  colSolver= new c.SimplexSolver()
+	colSolver.addConstraint( colEq)
 	return {
-		solver,
-		cols
+		solver: colSolver,
+		cols,
+		d,
+		x,
+		y	
 	}
 }
 
