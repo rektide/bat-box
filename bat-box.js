3e1ba1e673cc12879c7b313523ac2703da8267cc
rektide <rektide@voodoowarez.com>
2018-01-08 01:18:41 -0500

    Calculate free space. Create a 3D fitter. Create a main.

--- bat-box.js
+++ bat-box.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 "use strict"
 
 var
@@ -7,14 +8,13 @@ var
 /**
 * Return how circles of radius r fit into an x,y sized rectangle
 */
-function fit( d, x, y){
+function fit2D( d, x, y){
 	// find columns
 	var
 	  cols= new c.Variable({ name: "cols"}),
 	  colEq= new c.Equation( c.times( d, cols), x, c.Strength.required, 0),
 	  colSolver= new c.SimplexSolver()
 	colSolver.addConstraint( colEq)
-
 
 	// find rows
 	var
@@ -29,15 +29,71 @@ function fit( d, x, y){
 	  floorCols= Math.floor( cols.value),
 	  floorRows= Math.floor( rows.value),
 	  unpruned= floorCols* floorRows,
+	  insetTrim= ( cols.value- floorCols)< 0.5, // does inset need to be trimmed?
 	  insetRows= Math.floor( rows.value/ 2), // how many inset rows are there?
-	  insetTrim= ( cols.value - floorCols)< 0.5, // does inset need to be trimmed?
 	  trim= insetTrim&& insetRows|| 0,
-	  count= unpruned- trim
+	  count= unpruned- trim,
+	  colSize= floorCols+ (insetTrim? 0: 0.5) // physical size of cols
+
+	// calculate free space
+	var
+	  freeX= x- (d* colSize),
+	  freeY= y- ((d* diag* (floorRows- 1))+ d)
 	return {
+		count,
 		cols: cols.value,
 		rows: rows.value,
-		count
+		depth: 0,
+		x,
+		freeX,
+		y,
+		freeY,
+		z: 0,
+		freeZ: 0
 	}
 }
 
-module.exports= fit
+
+/**
+* Return permutations of how cylinders of d,h fit into an x,y,z sized space
+*/
+function fit3D( d, h, x, y, z){
+	// three possible orientations
+	var
+	  a= fit2D( d, x, y),
+	  b= fit2D( d, x, z),
+	  c= fit2D( d, y, z)
+	// calculate how many deep
+	a.z= z
+	a.depth= Math.floor( z/ h)
+	a.freeZ= z- ( a.depth* h)
+	a.count*= a.depth
+
+	b.z= y
+	b.depth= Math.floor( y/ h)
+	b.freeZ= y- ( b.depth* h)
+	b.count*= b.depth
+
+	c.z= x
+	c.depth= Math.floor( x/ h)
+	c.freeZ= x- ( c.depth* h)
+	c.count*= c.depth
+
+	return { a, b, c}
+}
+
+
+module.exports.fit2D= fit2D
+module.exports.fit3D= fit3D
+module.exports= fit3D
+
+if( require.main=== module){
+	var args= new Array( 5)
+	for( var i= 0; i< args.length; ++i){
+		args[ i]= Number.parseFloat( process.argv[ i+ 2])
+	}
+	var [ d, h, x, y, z]= args
+	console.log({ d, h, x, y, z})
+	var calc= fit3D.apply( null, args)
+	console.log( JSON.stringify( calc, null, "\t"))
+}
